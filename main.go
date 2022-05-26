eb6e018e60981c423d02a609666b55ad7ee60146
ferizoozoo <farhadzohoorz@gmail.com>
2022-05-26 10:21:16 +0430

    Test challenge 3, set 1 after changing to return and receive byte array values

--- main.go
+++ main.go
@@ -8,10 +8,11 @@ import (
 
 func main() {
 	// fmt.Println(string(cryptopals.HexToBase64String("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")))
-	msg1, _ := hex.DecodeString("1c0111001f010100061a024b53535009181c")
-	msg2, _ := hex.DecodeString("686974207468652062756c6c277320657965")
-	fmt.Println(string(cryptopals.FixedXor(msg1, msg2)))
-	// fmt.Printf("%s", cryptopals.FindCorrectPlainTextWithSingleByteXorCipher("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"))
+	// msg1, _ := hex.DecodeString("1c0111001f010100061a024b53535009181c")
+	// msg2, _ := hex.DecodeString("686974207468652062756c6c277320657965")
+	// fmt.Println(string(cryptopals.FixedXor(msg1, msg2)))
+	msg, _ := hex.DecodeString("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
+	fmt.Printf("%s", cryptopals.FindCorrectPlainTextWithSingleByteXorCipher(msg))
 
 	// cryptopals.DetectSingleCharacterXor()
 	// fmt.Println(string(cryptopals.RepeatingKeyXorEncryption([]byte("Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"), []byte("ICE"))))
